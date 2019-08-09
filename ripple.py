9ba376e0475954744b671b34a2b548fd539835c5
salt-die <naclmolecule@gmail.com>
2019-08-09 03:56:17 -0500

    PEP conformity.

--- ripple.py
+++ ripple.py
@@ -8,50 +8,64 @@ import pygame
 from pygame.mouse import get_pos
 
 def ripples():
+    """
+    Simulates ripples on a surface.
+    """
 
     def update_array():
+        """
+        Ripple physics.
+        """
         def pad_with(vector, pad_width, iaxis, kwargs):
+            """
+            Extra arguments needed for compatibility with numpy.
+            """
             vector[:pad_width[0]] = 0
             vector[-pad_width[1]:] = 0
 
         nonlocal surface_array
         old_array = pad(surface_array, 1, pad_with) #pad borders with zeros
-        shift_left = concatenate((old_array.T[-1:],old_array.T[:-1])).T
-        shift_right = concatenate((old_array.T[1:],old_array.T[:1])).T
-        shift_up = concatenate((old_array[1:],old_array[:1]))
-        shift_down = concatenate((old_array[-1:],old_array[:-1]))
+        shift_left = concatenate((old_array.T[-1:], old_array.T[:-1])).T
+        shift_right = concatenate((old_array.T[1:], old_array.T[:1])).T
+        shift_up = concatenate((old_array[1:], old_array[:1]))
+        shift_down = concatenate((old_array[-1:], old_array[:-1]))
 
         surface_array = ((shift_left +\
                           shift_right +\
                           shift_up +\
-                          shift_down) / 2)[1:-1,1:-1] - surface_array
+                          shift_down) / 2)[1:-1, 1:-1] - surface_array
 
-        surface_array *= .5 #damp waves
+        surface_array *= .340 #damp waves
+        clip(surface_array, -scale/2, scale/2, surface_array)
+        #surface_array[abs(surface_array) < .1] = 0
 
     def color(surface_array):
-        scale = 100
-        clipped = clip(surface_array, -scale // 2, scale // 2)
-        clipped += scale // 2
+        """
+        Returns colors based on the values of surface_array.
+        """
         color_1 = (65, 234, 186)
         color_2 = (13, 29, 135)
-        return dstack([(clipped * (c2 - c1) / scale + c1).astype(int)\
+        return dstack([(surface_array * (c2 - c1) / scale + c1).astype(int)\
                        for c1, c2 in zip(color_1, color_2)])
 
     def get_user_input():
+        """
+        Takes care of clicks and close events.
+        """
         for event in pygame.event.get():
             if event.type == 12: #quit
                 nonlocal running
                 running = False
             elif event.type == 5: #mouse down
                 if event.button == 1: #left-Click
-                    surface_array[get_pos()] +=100
+                    surface_array[get_pos()] += 100
 
     #Game variables-----------------------------------------------------------
-    window_dim = array([100.0, 100.0])
+    window_dim = array([500.0, 500.0])
     window = pygame.display.set_mode(window_dim.astype(int))
-    surface_array = zeros((int(window_dim[0]),int(window_dim[1])))
+    surface_array = zeros((int(window_dim[0]), int(window_dim[1])))
     clock = pygame.time.Clock() #For limiting fps
-
+    scale = 100
     #Main Loop----------------------------------------------------------------
     running = True
     while running:
